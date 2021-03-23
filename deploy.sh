bc5adae095d216ffc1013742297c34e5fcb22fb8
Robson Diego <robdig7x@gmail.com>
2021-03-23 17:20:07 -0300

    gitignore

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,12 @@
 # build
 npm run build
 
-# navigate into the build output directory
-cd dist
-
+cd dist 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
 git add .
-git commit -m 'deploy'
+git commit -m 'deploy => dist subtree'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
@@ -22,8 +19,8 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # https://robdig7x.github.io/vue-oficina-mobile/
 #6f3ef0f060f3d59f39b8075e69cce2b938c8149d
-git remote add origin git@github.com:robdig7x/vue-oficina-mobile.git
-git push -u origin gh-pages
+git push origin --delete gh-pages
+git subtree push --prefix dist origin gh-pages
 
 #git push -f git@github.com:robdig7x/vue-oficina-mobile.git master:gh-pages
 
